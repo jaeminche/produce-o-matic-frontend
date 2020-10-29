38078dfb8412b5982418a8b5f644f08d80271558
Jae M. Choi <jaeminche@gmail.com>
2020-10-29 16:39:26 +0900

    feat: create IntroTextContainer

--- src/components/common/IntroText.js
+++ src/components/common/IntroText.js
@@ -2,6 +2,7 @@ import React, { useRef, useEffect, useCallback } from 'react';
 import styled from 'styled-components/macro';
 import palette from '../../lib/styles/palette';
 import Responsive from '../common/Responsive';
+import ReactHtmlParser from 'react-html-parser';
 
 const IntroTextBlock = styled.div`
   height: auto;
@@ -9,12 +10,16 @@ const IntroTextBlock = styled.div`
 
 const Wrapper = styled(Responsive)`
   padding: 0;
+  text-align: center;
+  margin-top: 80px;
+  margin-bottom: 80px;
 `;
 
 const IntroText = ({ text }) => {
+  console.log('text', text);
   return (
     <IntroTextBlock>
-      <Wrapper>{text}</Wrapper>
+      <Wrapper>{ReactHtmlParser(text)}</Wrapper>
     </IntroTextBlock>
   );
 };
--- src/containers/main/IntroTextContainer.js
+++ src/containers/main/IntroTextContainer.js
@@ -2,12 +2,10 @@ import React, { useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
 import IntroText from '../../components/common/IntroText';
 
-const introtext = `A total solution from pre-production to post-production.
-The most efficient way to start your production.
-Easy, fast, accurate and transparent`;
+const text = `<p>A total solution from pre-production to post-production.</p><p>The most efficient way to start your production.</p><p>Easy, fast, accurate and transparent</p>`;
 
 const IntroTextContainer = () => {
-  return <IntroText text={introtext} />;
+  return <IntroText text={text} />;
 };
 
 export default withRouter(IntroTextContainer);
