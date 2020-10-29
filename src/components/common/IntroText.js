838161d209015d68c4a7c208c5bb71b6f4b8164f
Jae M. Choi <jaeminche@gmail.com>
2020-10-29 17:12:07 +0900

    feat: add 'view more >' to IntroText

--- src/components/common/IntroText.js
+++ src/components/common/IntroText.js
@@ -1,4 +1,5 @@
 import React, { useRef, useEffect, useCallback } from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components/macro';
 import palette from '../../lib/styles/palette';
 import Responsive from '../common/Responsive';
@@ -13,13 +14,25 @@ const Wrapper = styled(Responsive)`
   text-align: center;
   margin-top: 80px;
   margin-bottom: 80px;
+  font-size: 24px;
+
+  .viewmore {
+    color: rgba(37, 168, 209, 1);
+  }
 `;
 
 const IntroText = ({ text }) => {
   console.log('text', text);
   return (
     <IntroTextBlock>
-      <Wrapper>{ReactHtmlParser(text)}</Wrapper>
+      <Wrapper>
+        <>
+          {ReactHtmlParser(text)}
+          <Link to="/" className="viewmore">
+            View More >
+          </Link>
+        </>
+      </Wrapper>
     </IntroTextBlock>
   );
 };
