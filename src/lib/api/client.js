4280f45fd938f6d5d554067b8361fa245de521f1
Jae M. Choi <jaeminche@gmail.com>
2020-11-23 20:56:15 +0900

    feat: change host url for aws

--- src/lib/api/client.js
+++ src/lib/api/client.js
@@ -1,6 +1,23 @@
 import axios from 'axios';
 
-const client = axios.create();
+const env = process.env.NODE_ENV || 'development';
+console.log('env', env);
+
+const host = env === 'production' ? 'http://13.124.187.20/' : null;
+const option = host && {
+  baseURL: `${host}`,
+};
+
+const client = option ? axios.create(option) : axios.create();
+
+// client.interceptors.request.use(
+//   function (config) {
+//     console.log('config', config);
+//   },
+//   function (error) {
+//     return Promise.reject(error);
+//   },
+// );
 
 /** client.defaults.baseURL = 'https://';
 
