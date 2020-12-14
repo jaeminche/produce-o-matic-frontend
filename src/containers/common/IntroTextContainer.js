f116ecc0a05c9734bbca3fe9c732469fcfa2f850
Jae M. Choi <jaeminche@gmail.com>
2020-12-14 13:25:40 +0900

    fix: intro text

--- src/containers/common/IntroTextContainer.js
+++ src/containers/common/IntroTextContainer.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 import IntroText from '../../components/common/IntroText';
 
-const text = `<div>A total solution from pre-production to post-production.</div><div>The most efficient way to start your production.</div><div>Easy, fast, accurate and transparent</div>`;
+// const text = `<div>A total solution from pre-production to post-production.</div><div>The most efficient way to start your production.</div><div>Easy, fast, accurate and transparent</div>`;
+
+const text = `<div>Budget-O-matic is an easy and fast budget calculator.</div><div>Check the estimated budget for your film/video production in Korea.</div><div>(Including incentive program up to 30%)</div>`;
 
 const IntroTextContainer = () => {
   return <IntroText text={text} />;
