2b09d18c83570615b7cc5cd92da6beb020514977
Jae M. Choi <jaeminche@gmail.com>
2021-01-19 16:18:07 +0900

    feat: making (testing done) GoogleDocContainer

--- src/containers/myGoogleDoc/MyGoogleDocContainer.js
+++ src/containers/myGoogleDoc/MyGoogleDocContainer.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useMediaQuery } from 'react-responsive';
-import { withRouter } from 'react-router-dom';
+import { withRouter, Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { uiData } from '../../lib/constants/uiData';
 import Contents from '../../components/common/Contents';
@@ -10,14 +10,56 @@ import {
   getUsersLocation,
   postCopyContractSample,
 } from '../../modules/thirdPartyApis';
+import { DOC } from '../../assets';
+import GoogleDocsViewer from 'react-google-docs-viewer';
 
-const MyGoogleDocContainer = () => {
+const DocIframe = ({ source }) => {
+  if (!source) {
+    return <div>Loading...</div>;
+  }
+
+  const src = source;
+  return (
+    <div>
+      <iframe
+        src={'https://docs.google.com/viewer?url=' + src + '&embedded=true'}
+        title="file"
+        width="50%"
+        height="600"
+      ></iframe>
+    </div>
+  );
+};
+
+const MyGoogleDocContainer = ({ history }) => {
   const dispatch = useDispatch();
   const fileId = '12O4bV8sJj43LAtMXY3FRknaHs56eZqJyju5d9i2JKBQ';
+  console.log('===922', DOC);
   return (
-    <button onClick={() => dispatch(postCopyContractSample({ fileId }))}>
-      계약서 샘플 복사하기
-    </button>
+    <>
+      <div>
+        <a
+          href="https://docs.google.com/document/d/1hdQDYfiNGHOTDiEPUSsOmVBRYEBjPWNihSrsmza3WSU/edit?usp=sharing"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <iframe
+            title="Local Production Crew Agreement Sample"
+            src="https://docs.google.com/document/d/e/2PACX-1vRPbg1_7vlxLDqmmWjPbAW44ny7lcXXlr4Tc8QdAaUxGnTeZ67A6UXa7ceZKbNZTTw2b-cQBoT50kxs/pub?embedded=true"
+            // scrolling="no"
+            frameborder="1"
+            marginheight="0px"
+            marginwidth="0px"
+            height="400px"
+            width="600px"
+            allowfullscreen
+          ></iframe>
+        </a>
+      </div>
+      {/* <button onClick={() => dispatch(postCopyContractSample({ fileId }))}>
+        계약서 샘플 복사하기
+      </button> */}
+    </>
   );
 };
 
