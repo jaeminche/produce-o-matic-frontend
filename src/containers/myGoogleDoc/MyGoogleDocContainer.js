484e123c8cd2248ed668a0dc724efa4af628e8da
Jae M. Choi <jaeminche@gmail.com>
2021-01-20 01:23:15 +0900

    feat: make Google Doc Container reusable

--- src/containers/myGoogleDoc/MyGoogleDocContainer.js
+++ src/containers/myGoogleDoc/MyGoogleDocContainer.js
@@ -4,7 +4,8 @@ import { withRouter } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { uiData } from '../../lib/constants/uiData';
 import Contents from '../../components/common/Contents';
-import { formatTime } from '../../lib/format';
+import styled from 'styled-components/macro';
+import Responsive from '../../components/common/Responsive';
 import {
   getIp,
   getUsersLocation,
@@ -13,40 +14,44 @@ import {
 // import { Card } from '@coreui/icons';
 import { Card, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../../lib/styles/customCss.css';
+import '../../lib/styles/customCssToOverrideBootstrap.css';
 import { THUMBNAIL_LOCALPRODUCTIONCREWAGREEMENT } from '../../assets';
 
-const MyGoogleDocContainer = ({ history }) => {
-  const dispatch = useDispatch();
-  const fileId = '12O4bV8sJj43LAtMXY3FRknaHs56eZqJyju5d9i2JKBQ';
+const MyGoogleDocContainerBlock = styled.div`
+  height: auto;
+`;
+
+const Wrapper = styled(Responsive)`
+  text-align: left;
+  padding-top: 80px;
+`;
+
+const RowWrap = styled.div`
+  display: -webkit-box;
+  display: -webkit-flex;
+  display: flex;
+  -webkit-flex-wrap: wrap;
+  flex-wrap: wrap;
+  height: 100%;
+  margin: -5px;
+  overflow: hidden;
+
+  justify-content: space-between;
+`;
+
+const MyCardTitle = styled.span`
+  color: rgba(0, 0, 0, 0.72);
+  line-height: 20px;
+  margin-left: 8px;
+  padding-bottom: 4px;
+  font-size: 18px;
+`;
+
+const MyCard = (props) => {
+  const { title, imgUrl, redirectTo } = props;
   return (
-    <div style={{ width: '300px', height: '209px', position: 'relative' }}>
+    <div style={{ width: '300px', height: '218px', position: 'relative' }}>
       <Card style={{ width: '95%', margin: '0 auto' }}>
-        {/* <div
-          style={{
-            backgroundColor: '#f5f5f5',
-            padding: '1.25rem',
-            height: '118px',
-          }}
-        >
-          <a
-            href="https://docs.google.com/document/d/1hdQDYfiNGHOTDiEPUSsOmVBRYEBjPWNihSrsmza3WSU/edit?usp=sharing"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            <iframe
-              title="Local Production Crew Agreement Sample"
-              src="https://docs.google.com/document/d/e/2PACX-1vRPbg1_7vlxLDqmmWjPbAW44ny7lcXXlr4Tc8QdAaUxGnTeZ67A6UXa7ceZKbNZTTw2b-cQBoT50kxs/pub?embedded=true"
-              // scrolling="no"
-              frameborder="0"
-              marginheight="0px"
-              marginwidth="0px"
-              height="inherit"
-              width="90%"
-              // allowfullscreen
-            ></iframe>
-          </a>
-        </div> */}
         <div
           style={{
             backgroundColor: '#f5f5f5',
@@ -55,26 +60,69 @@ const MyGoogleDocContainer = ({ history }) => {
         >
           <Card.Img
             variant="top"
-            src={THUMBNAIL_LOCALPRODUCTIONCREWAGREEMENT}
+            src={imgUrl}
             style={{
-              boxShadow: '0 0 6px 0 rgba(193, 193, 193, 0.75)',
+              boxShadow: '0 0 6px 0 rgba(0,0,0,0.12)',
             }}
           />
         </div>
         <Card.Body style={{ color: 'black' }}>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
+          <a href={redirectTo} rel="noopener noreferrer" target="_blank">
+            <Card.Title>
+              <img
+                src="https://drive-thirdparty.googleusercontent.com/16/type/application/vnd.google-apps.document"
+                alt="Google Docs"
+              />
+              <MyCardTitle>{title}</MyCardTitle>
+            </Card.Title>
+          </a>
         </Card.Body>
       </Card>
 
       {/* <button onClick={() => dispatch(postCopyContractSample({ fileId }))}>
-        계약서 샘플 복사하기
-      </button> */}
+  계약서 샘플 복사하기
+</button> */}
     </div>
+  );
+};
+
+const MyGoogleDocContainer = ({ history }) => {
+  const dispatch = useDispatch();
+  const fileId = '12O4bV8sJj43LAtMXY3FRknaHs56eZqJyju5d9i2JKBQ';
+  const SAMPLES = [
+    {
+      title: 'LOCAL PRODUCTION CREW AGREEMENT SAMPLE',
+      imgUrl: THUMBNAIL_LOCALPRODUCTIONCREWAGREEMENT,
+      redirectTo:
+        'https://docs.google.com/document/d/1hdQDYfiNGHOTDiEPUSsOmVBRYEBjPWNihSrsmza3WSU/edit?usp=sharing',
+    },
+    {
+      title: 'LOCAL PRODUCTION CREW AGREEMENT SAMPLE',
+      imgUrl: THUMBNAIL_LOCALPRODUCTIONCREWAGREEMENT,
+      redirectTo:
+        'https://docs.google.com/document/d/1hdQDYfiNGHOTDiEPUSsOmVBRYEBjPWNihSrsmza3WSU/edit?usp=sharing',
+    },
+    {
+      title: 'LOCAL PRODUCTION CREW AGREEMENT SAMPLE',
+      imgUrl: THUMBNAIL_LOCALPRODUCTIONCREWAGREEMENT,
+      redirectTo:
+        'https://docs.google.com/document/d/1hdQDYfiNGHOTDiEPUSsOmVBRYEBjPWNihSrsmza3WSU/edit?usp=sharing',
+    },
+  ];
+  return (
+    <MyGoogleDocContainerBlock>
+      <Wrapper>
+        <RowWrap>
+          {SAMPLES.map((sample) => (
+            <MyCard
+              title={sample.title}
+              imgUrl={sample.imgUrl}
+              redirectTo={sample.redirectTo}
+            />
+          ))}
+        </RowWrap>
+      </Wrapper>
+    </MyGoogleDocContainerBlock>
   );
 };
 
