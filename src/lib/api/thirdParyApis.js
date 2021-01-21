f0677d3de1e7867b380b1a6cc838de38a8156072
Jae M. Choi <jaeminche@gmail.com>
2021-01-21 22:57:50 +0900

    doc: add doc to getUserLocation api

--- src/lib/api/thirdParyApis.js
+++ src/lib/api/thirdParyApis.js
@@ -8,6 +8,7 @@ export const getUsersLocation = ({ ip }) =>
   // clientForExternalApi.get(`http://ip-api.com/json/${ip}`); // insecure request
   // clientForExternalApi.get(`https://geolocation-db.com/json`); // returns city: null
   clientForExternalApi.get(`https://extreme-ip-lookup.com/json`); // returns incorrect, but nearest city name
+// ! ip-api.com 제외한 서비스를 이용할 경우엔 getIp 호출 필요없음. 그리고 ip 서비스에서 lat, long 반환하는 경우, 날씨위젯을 위한 위치 정보 동의 구하지 않아도 됨.
 
 export const postCopyContractSample = ({ fileId }) =>
   clientForExternalApi.post(
