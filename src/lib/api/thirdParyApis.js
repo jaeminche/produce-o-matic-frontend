311726a2a02d4f610eef50cd9381bc6ac2118f13
Jae M. Choi <jaeminche@gmail.com>
2021-01-21 18:36:09 +0900

    feat: apply clientForExternalApi

--- src/lib/api/thirdParyApis.js
+++ src/lib/api/thirdParyApis.js
@@ -1,13 +1,18 @@
 // import qs from 'qs';
-import client from './client';
+import clientForExtenalApi from './clientForExtenalApi';
 
-export const getIp = () => client.get(`https://api.ipify.org?format=json`);
+export const getIp = () =>
+  clientForExtenalApi.get(`https://api.ipify.org?format=json`);
 
 export const getUsersLocation = ({ ip }) =>
-  client.get(`http://ip-api.com/json/${ip}`);
+  clientForExtenalApi.get(`http://ip-api.com/json/${ip}`);
 
 export const postCopyContractSample = ({ fileId }) =>
-  client.post(`https://www.googleapis.com/drive/v3/files/${fileId}/copy`);
+  clientForExtenalApi.post(
+    `https://www.googleapis.com/drive/v3/files/${fileId}/copy`,
+  );
 
 export const getCurrencySet = ({ apikey }) =>
-  client.get(`http://data.fixer.io/api/latest?access_key=${apikey}`);
+  clientForExtenalApi.get(
+    `http://data.fixer.io/api/latest?access_key=${apikey}`,
+  );
