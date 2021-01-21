d7bd7c4f31c3677c00268f2c83aec6ee35cb8c1a
Jae M. Choi <jaeminche@gmail.com>
2021-01-21 18:47:10 +0900

    fix: minor change: typo

--- src/lib/api/thirdParyApis.js
+++ src/lib/api/thirdParyApis.js
@@ -1,18 +1,18 @@
 // import qs from 'qs';
-import clientForExtenalApi from './clientForExtenalApi';
+import clientForExternalApi from './clientForExternalApi';
 
 export const getIp = () =>
-  clientForExtenalApi.get(`https://api.ipify.org?format=json`);
+  clientForExternalApi.get(`https://api.ipify.org?format=json`);
 
 export const getUsersLocation = ({ ip }) =>
-  clientForExtenalApi.get(`http://ip-api.com/json/${ip}`);
+  clientForExternalApi.get(`http://ip-api.com/json/${ip}`);
 
 export const postCopyContractSample = ({ fileId }) =>
-  clientForExtenalApi.post(
+  clientForExternalApi.post(
     `https://www.googleapis.com/drive/v3/files/${fileId}/copy`,
   );
 
 export const getCurrencySet = ({ apikey }) =>
-  clientForExtenalApi.get(
+  clientForExternalApi.get(
     `http://data.fixer.io/api/latest?access_key=${apikey}`,
   );
