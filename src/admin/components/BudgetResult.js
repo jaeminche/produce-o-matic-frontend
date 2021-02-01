7d6592433341492e78ea26e82234efde6a8b721f
Jae M. Choi <jaeminche@gmail.com>
2021-02-01 22:37:56 +0900

    feat: [admin] creating BudgetResult page

--- src/admin/components/BudgetResult.js
+++ src/admin/components/BudgetResult.js
@@ -3,8 +3,8 @@ import Table from '../components/common/Table';
 
 const BudgetResult = (props) => {
   const { targetResult } = props;
-  const { result, uuid, _id, createdAt_local, createdAt_utc } = targetResult;
-  return <div>{/* <BudgetResult /> */}</div>;
+  // const { result, uuid, _id, createdAt_local, createdAt_utc } = targetResult;
+  return <div>{targetResult && targetResult._id}</div>;
 };
 
 export default BudgetResult;
