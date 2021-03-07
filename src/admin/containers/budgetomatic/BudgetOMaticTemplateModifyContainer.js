5f319e9448770aebc0603bf0b44c3cc30d7f32e6
Jae M. Choi <jaeminche@gmail.com>
2021-03-08 01:56:32 +0900

    fix: updateItem/group/category 저장 성공 후 listeItemGroups() 호출 조건 재설정

--- src/admin/containers/budgetomatic/BudgetOMaticTemplateModifyContainer.js
+++ src/admin/containers/budgetomatic/BudgetOMaticTemplateModifyContainer.js
@@ -258,6 +258,7 @@ const BudgetOMaticTemplateModifyContainer = (props) => {
       // ? initialize forms and refresh the list
       dispatch(initializeForm('updateItemSubmitted'));
       toggleUpdateItem(groupCode, key);
+      dispatch(listItemsGroups());
     } else if (updateGroupSubmitted && updateGroupSubmitted.code) {
       myToast(
         `제출 성공. 템플릿의 그룹 코드 ${updateGroupSubmitted.code}가 업데이트되었습니다.`,
@@ -265,15 +266,16 @@ const BudgetOMaticTemplateModifyContainer = (props) => {
       // ? initialize forms and refresh the list
       dispatch(initializeForm('updateGroupSubmitted'));
       toggleUpdateGroup(activeGroup);
+      dispatch(listItemsGroups());
     } else if (updateCategorySubmitted && updateCategorySubmitted.name) {
       myToast(
         `제출 성공. 템플릿의 카테고리 이름 ${updateCategorySubmitted.name}가 업데이트되었습니다.`,
       );
       // ? initialize forms and refresh the list
       dispatch(initializeForm('updateCategorySubmitted'));
+      dispatch(listItemsGroups());
       // toggleUpdateCategory?();
     }
-    dispatch(listItemsGroups());
   }, [updateItemSubmitted, updateGroupSubmitted, updateCategorySubmitted]);
 
   useEffect(() => {
