c9f27ed30a809f9313d8e6670c798afefd3643c2
Jae M. Choi <jaeminche@gmail.com>
2021-03-12 13:16:35 +0900

    feat: apply BasicDropzone to PopularLocations

--- src/admin/components/contents/PopularLocations.js
+++ src/admin/components/contents/PopularLocations.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import BasicDropzone from '../../../components/BasicDropzone/BasicDropzone';
 // import PopularLocationsTable from './tables/PopularLocationsTable';
 
 const PopularLocations = (props) => {
   //   console.log('==7979', props.PopularLocations);
   //   return <PopularLocationsTable {...props} />;
-  return <div>test</div>;
+  return (
+    <div>
+      <BasicDropzone />
+    </div>
+  );
 };
 
 export default PopularLocations;
