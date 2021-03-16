6e69b5c6688c41cae707429d3f85caa35ef8d63b
Jae M. Choi <jaeminche@gmail.com>
2021-03-16 15:16:26 +0900

    refactor: change alert() to myToast()

--- src/admin/containers/contents/PopularLocationsContainer.js
+++ src/admin/containers/contents/PopularLocationsContainer.js
@@ -4,6 +4,7 @@ import { withRouter, useHistory } from 'react-router-dom';
 // import { listBudgetResults } from '../../../modules/budgetResults';
 import clientForMultipart from '../../../lib/api/clientForMultipart';
 import PopularLocations from '../../components/contents/PopularLocations';
+import { myToast } from '../../../lib/util/myToast';
 
 const PopularLocationsContainer = () => {
   const dispatch = useDispatch();
@@ -26,46 +27,50 @@ const PopularLocationsContainer = () => {
       .then(function (response) {
         console.log('==999.0', /*response.data.results[0].image,*/ response);
         if (
-          !(
-            (
-              response.status === 200 &&
-              response.data &&
-              response.data === 'done'
-            ) //!여기
-          )
+          response.status === 200 &&
+          response.data &&
+          response.data.contentType
         ) {
-          alert('File uploading failed! Consult your engineer.');
+          if (response.data.contentType === 'image/png') {
+            myToast(`File uploading successful!`);
+          } else {
+            myToast(`이미지 파일만 업로드 가능합니다`);
+          }
+        } else {
+          myToast(`File uploading failed! Consult your engineer.`);
+          return;
+
+          // onChange({
+          //   name:
+          //     type === 'I0'
+          //       ? 'verify_image_path'
+          //       : type === 'legal_represent_file_path'
+          //       ? 'legal_represent_file_path'
+          //       : type === 'corp_biz_file_path'
+          //       ? 'corp_biz_file_path'
+          //       : type,
+          //   value: response.data.results[0].image,
+          //   formindex,
+          // });
+          // dispatch(
+          //   changeField({
+          //     form: "creditorInfoForm",
+          //     key:
+          //       type === "I0"
+          //         ? "verify_image_path"
+          //         : type === "legal_represent_file_path"
+          //         ? "legal_represent_file_path"
+          //         : type === "corp_biz_file_path"
+          //         ? "corp_biz_file_path"
+          //         : "",
+          //     value: response.data.results[0].image,
+          //     formindex
+          //   })
+          // );
         }
-        // onChange({
-        //   name:
-        //     type === 'I0'
-        //       ? 'verify_image_path'
-        //       : type === 'legal_represent_file_path'
-        //       ? 'legal_represent_file_path'
-        //       : type === 'corp_biz_file_path'
-        //       ? 'corp_biz_file_path'
-        //       : type,
-        //   value: response.data.results[0].image,
-        //   formindex,
-        // });
-        // dispatch(
-        //   changeField({
-        //     form: "creditorInfoForm",
-        //     key:
-        //       type === "I0"
-        //         ? "verify_image_path"
-        //         : type === "legal_represent_file_path"
-        //         ? "legal_represent_file_path"
-        //         : type === "corp_biz_file_path"
-        //         ? "corp_biz_file_path"
-        //         : "",
-        //     value: response.data.results[0].image,
-        //     formindex
-        //   })
-        // );
       })
       .catch(function (response) {
-        alert(`서버와의 통신 중에 오류 발생 : ${response}`);
+        myToast(`서버와의 통신 중에 오류 발생 : ${response}`);
         console.log('---401.1', response);
         if (response.status === 401) {
           //   refreshAndSetJwtAndLoginType();
