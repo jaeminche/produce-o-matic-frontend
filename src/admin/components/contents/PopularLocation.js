7f14d017ea31f73927c59d1071de92a707bd6c5e
Jae M. Choi <jaeminche@gmail.com>
2021-03-23 00:45:29 +0900

    fix: change popularlocations UI (including submit button)

--- src/admin/components/contents/PopularLocation.js
+++ src/admin/components/contents/PopularLocation.js
@@ -3,7 +3,15 @@ import { useDispatch } from 'react-redux';
 import BasicDropzone from '../../../components/BasicDropzone/BasicDropzone';
 // import PopularLocationsTable from './tables/PopularLocationsTable';
 import { listPopularLocations } from '../../../modules/popularLocations';
-import { CForm } from '@coreui/react';
+import {
+  CCard,
+  CCardHeader,
+  CCardBody,
+  CForm,
+  CButton,
+  CCardFooter,
+} from '@coreui/react';
+import CIcon from '@coreui/icons-react';
 import { ContentsFormGroup } from '../../../admin/components/common/FormGroups';
 
 const PopularLocation = (props) => {
@@ -20,19 +28,40 @@ const PopularLocation = (props) => {
   //   return <PopularLocationsTable {...props} />;
 
   return (
-    <div>
-      <CForm
-        action=""
-        method="post"
-        encType="multipart/form-data"
-        className="form-horizontal"
-        style={{ marginRight: '15px' }}
-      >
-        <ContentsFormGroup {...props} />
-      </CForm>
-      <BasicDropzone {...props} />
-      {formDataReady && <input onClick={() => onSubmit({ type: 'patch' })} />}
-    </div>
+    <CCard>
+      <CCardHeader>
+        <p>
+          앞서 선택하신 Location의 세부 정보를 변경하고 Submit 버튼을
+          눌러주세요.
+        </p>
+        <p>Submit 버튼을 누르기 전에는 변경사항이 저장되지 않습니다.</p>
+      </CCardHeader>
+      <CCardBody>
+        <CForm
+          action=""
+          method="post"
+          encType="multipart/form-data"
+          className="form-horizontal"
+          style={{ marginRight: '15px' }}
+        >
+          <ContentsFormGroup {...props} />
+        </CForm>
+        <BasicDropzone {...props} />
+      </CCardBody>
+      <CCardFooter>
+        <CButton
+          onClick={() => (formDataReady ? onSubmit({ type: 'patch' }) : null)}
+          type="submit"
+          size="sm"
+          color="primary"
+        >
+          <CIcon name="cil-scrubber" /> Submit
+        </CButton>
+        {/* <CButton type="reset" size="sm" color="danger">
+          <CIcon name="cil-ban" /> Reset
+        </CButton> */}
+      </CCardFooter>
+    </CCard>
   );
 };
 
